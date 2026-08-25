38d0f2f7fcd9cc15f5a2910114f6eba179640b8d
Fong <phamthanhphong160607@gmail.com>
2026-08-25 22:38:18 +0700

    Script for deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,12 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import "dotenv/config"
+import path from 'path'
 
 import userRoutes from "./routes/user.route.js"
 
 const app = express()
+const __dirname = path.resolve()
 
 app.use(cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
@@ -13,6 +15,15 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(cookieParser())
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get(/^(?!\/api).*/, (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 app.get("/", (req, res) => {
     res.json({
